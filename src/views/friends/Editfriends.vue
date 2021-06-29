ffda7bd6eaebc0c24c3c36b0dd37d755c577a630
IndahNurulaeni <72368610+IndahNurulaeni@users.noreply.github.com>
2021-06-29 18:18:34 +0700

    commit9

--- src/views/friends/Editfriends.vue
+++ src/views/friends/Editfriends.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card shadow mt-3">
     <div class="card-body">
-      <h5 class="card-title">Edit Friends</h5>
+      <h5 class="card-title">Edit friend</h5>
       <form class="row g-3" @submit.prevent="update">
         <div class="col-md-6">
           <label for="inputEmail4" class="form-label">Nama</label>
@@ -33,14 +33,13 @@
             type="text"
             class="form-control"
             id="inputAddress"
-            placeholder="Masukkan Alamat"
+            placeholder="Masukan alamat"
             v-model="friend.alamat"
           />
           <div class="alert alert-danger" v-if="validation.alamat">
             {{ validation.alamat[0] }}
           </div>
         </div>
-
         <div class="col-12">
           <button type="submit" class="btn btn-primary">Edit</button>
         </div>
@@ -49,38 +48,43 @@
   </div>
 </template>
 <script>
-import { onMounted, reactive, ref } from "vue";
+import {onMounted, reactive, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import axios from "axios";
 export default {
+
   setup() {
     const friend = reactive({
       nama: "",
       no_tlp: "",
       alamat: "",
     });
+
     const validation = ref([]);
+
     const router = useRouter();
-    const route = useRoute();
-    onMounted(() => {
-      axios
-        .get(`http://pia.labirin.co.id/api/friends/${route.params.id}`)
-        .then((Response) => {
-          console.log(Response.data.data.nama);
-          friend.nama = Response.data.data.nama;
-          friend.no_tlp = Response.data.data.no_tlp;
-          friend.alamat = Response.data.data.alamat;
-        })
-        .catch((error) => {
-          console.log(error.Response.data);
-        });
-    });
+
+    const route = useRoute()
+
+onMounted(()=>{
+  axios.get(`http://127.0.0.1:8000/api/friends/${route.params.id}`)
+  .then(response => {
+console.log(response.data.data.nama)
+
+    friend.nama = response.data.data.nama
+    friend.no_tlp = response.data.data.no_tlp
+    friend.alamat = response.data.data.alamat
+  }).catch(error =>{
+    console.log(error.response.data)
+  })
+})
+
     function update() {
       let nama = friend.nama;
       let no_tlp = friend.no_tlp;
       let alamat = friend.alamat;
-      axios
-        .put(`http://pia.labirin.co.id/api/friends/${route.params.id}`, {
+
+      axios.put(`http://127.0.0.1:8000/api/friends/${route.params.id}`, {
           nama: nama,
           no_tlp: no_tlp,
           alamat: alamat,
@@ -91,7 +95,7 @@ export default {
           });
         })
         .catch((error) => {
-          console.log(error);
+          validation.value = error.response.data
         });
     }
     return {
@@ -99,7 +103,7 @@ export default {
       validation,
       router,
       update,
-      route,
+      route
     };
   },
 };
